78471dd9d0d4c2b84d70eba547d079be52393e62
ZHAO Hong <zhaohong@coderprepares.com>
2020-10-21 11:51:45 +0800

    添加加载状态

--- src/button.vue
+++ src/button.vue
@@ -1,5 +1,6 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <g-icon name="loading" class="loading"></g-icon>
     <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
     <div class="content">
       <slot></slot>
@@ -23,6 +24,15 @@ export default {
 </script>
 
 <style lang="scss">
+@keyframes spin {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
 .g-button {
   display: inline-flex;
   justify-content: center;
@@ -66,6 +76,10 @@ export default {
 
   &:focus {
     outline: none;
+  }
+
+  .loading {
+    animation: spin 2s infinite linear;
   }
 }
 </style>
