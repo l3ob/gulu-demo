19d3c8b38c1695d6d8da473d4c255624eea204cb
ZHAO Hong <zhaohong@coderprepares.com>
2020-10-21 12:21:55 +0800

    添加click事件

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,7 @@
 <template>
-  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-    <g-icon name="loading" class="loading"></g-icon>
-    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')">
+    <g-icon name="loading" class="icon loading" v-if="loading"></g-icon>
+    <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
     <div class="content">
       <slot></slot>
     </div>
@@ -16,8 +16,12 @@ export default {
       type: String,
       default: 'left',
       validator(value) {
-        return !['left', 'right'].includes(value);
+        return ['left', 'right'].includes(value);
       }
+    },
+    loading: {
+      type: Boolean,
+      default: false
     }
   }
 }
